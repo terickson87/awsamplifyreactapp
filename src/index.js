969ca8cc28600bc670291af25255dd16d4dcbdd7
Todd Erickson <twericks@usc.edu>
2021-02-17 16:44:19 -0700

    tried to fix build error

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+import Amplify from '@aws-amplify/core'
+import { Auth } from '@aws-amplify/auth'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+Auth.configure(awsconfig)
 
 ReactDOM.render(
   <React.StrictMode>
